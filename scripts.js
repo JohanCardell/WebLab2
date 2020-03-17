8a570e5898222c5d18b9cf6ababa65aba0a37e66
Johan Cardell <Johan.Cardell@iths.se>
2020-03-17 22:33:21 +0100

    request key, disable button

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-let key = 'API-key';
-www.forverkliga.se/JavaScript/api/crud.php?requestKey;  // once you have a key, it is ok to store it in a variable
-const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+const baseUrl = 'www.forverkliga.se/JavaScript/api/crud.php?';
+const keyRequestUrl = "www.forverkliga.se/JavaScript/api/crud.php?requestKey";  // once you have a key, it is ok to store it in a variable
+const urlKey = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + currentKey;
 const title = ' ';
 const newTitle = ' ';
 const author = ' ';
 const newAuthor = ' ';
 const id = ' ';
-const create = baseUrl + `&op=insert&title=${title}&author=${author}`; 
+const keyRequest = baseUrl
+const create = urlKey + `&op=insert&title=${title}&author=${author}`; 
 //"status": "success", "id" : id generated
-const readRequest = baseUrl + '&op=select';
+const readRequest = urlKey + '&op=select';
 //returns JSON object with status success, "data": [{"id", "title", "author", "updated"}]
-const updateRequest = baseUrl + `op=update&id=${id}&title=${newTitle}&author=${newAuthor}`;
-const deleteRequest = baseUrl + `op=delete&id=${id}`;
+const updateRequest = urlKey + `op=update&id=${id}&title=${newTitle}&author=${newAuthor}`;
+const deleteRequest = urlKey + `op=delete&id=${id}`;
+var currentKey = localStorage.getItem('apiAccessKey');
 
-fetch(url).then(???)
+
+//fetch(url).then(???)
 // check if json.status is error/success
 
 const bookElements = document.getElementsByClassName('book-element');
@@ -23,8 +26,47 @@ bookArray.forEach((book)=>{
     bookElements.push
 });
 
+function addBook(){
+    let elem = document.getElementById('btn_add_book');
+    // btnTimeout(elem)
+}
+
 function showBooks() {
 
 }
 var element = document.getElementsByClassName('');
-bookArray.push(document.readRequest)
+bookArray.push(document.readRequest);
+
+// function showCurrentKey(){
+//     if(currentKey == null){
+//         return 'No current key'
+//     } else{
+//         return currentKey;
+
+//     }
+    
+// }
+function requestKey(){
+    
+    let elem = document.getElementById('current_key');
+    elem.innerHTML = 'Generating key...'
+    fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
+        .then(response => response.json())
+        .then((myJson) => {
+            if (currentKey == null) {
+                requestKey();
+            } else {
+                console.log(myJson.key);
+                localStorage.setItem('apiAccessKey', myJson.key);
+                console.log(localStorage.getItem('apiAccessKey'));
+                elem.innerHTML = myJson.key;
+            }
+    });
+    
+}
+
+function btnTimeout(button){
+    let buttons = document.getElementsByClassName("button");
+    buttons.disabled = true;
+    setTimeout(function(){buttons.disabled = false;}, 2000);
+}
